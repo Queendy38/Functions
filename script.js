536f32137d01034493916f9881e9b8f151d6d176
Queendy38 <42394077+Queendy38@users.noreply.github.com>
2018-09-11 23:01:53 -1000

    projecy done DX

--- script.js
+++ script.js
@@ -1,9 +1,48 @@
-var x;
-function getName(){
+//create object for each burger
+//display inside the div
+//create button at the bottom
+//when button is clicked, display price with tax 4.712%
 
-    var x = prompt("Enter Name");
-    
+var spicyBur = {
+    name:"SpicyBurger",
+    meat: "Beef",
+    veg: ["Lettuce", "tomato", "Onion"],
+    sauce: ["Mayo", "Sriracha"],
+    price: 12.95
 }
-function showMe(){
-    window.alert(x);
+
+var mildBur = {
+ name: "MildBurger",
+ meat: "Chicken",
+ veg: "Spinach",
+ sauce: ["Mustard", "Mayo"],
+ price: 10.95
 }
+
+function seeMenu(){
+    document.getElementById('cname').innerHTML = spicyBur.name;
+    document.getElementById('cmeat').innerHTML = spicyBur.meat;
+    document.getElementById('cveg').innerHTML = spicyBur.veg;
+    document.getElementById('csauce').innerHTML = spicyBur.sauce;
+    document.getElementById('cprice').innerHTML = spicyBur.price;
+   
+    document.getElementById('name').innerHTML = mildBur.name;
+    document.getElementById('meat').innerHTML = mildBur.meat;
+    document.getElementById('veg').innerHTML = mildBur.veg;
+    document.getElementById('sauce').innerHTML = mildBur.sauce;
+    document.getElementById('price').innerHTML = mildBur.price; 
+
+   
+}
+//seeMenu();
+
+function spicyP(){
+   var totalS = (spicyBur.price * 1.04712).toFixed(2);
+   document.getElementById('one').innerHTML = "Your Total is " + totalS;
+}
+
+function mildP(){
+    var totalM = (mildBur.price * 1.04712).toFixed(2);
+    document.getElementById('two').innerHTML = "Your Total is " + totalM;
+}
+
