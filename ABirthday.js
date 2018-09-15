42835f96db504a8d98ce655aee6395513099723d
Queendy38 <42394077+Queendy38@users.noreply.github.com>
2018-09-15 02:03:52 -1000

    birthday gift

---
+++ ABirthday.js
@@ -0,0 +1,6 @@
+function popWord(){
+    document.getElementById('one').innerHTML = "Happy Birthday!";
+    document.getElementById('two').innerHTML = "God Bless You!";
+    document.getElementById('three').innerHTML = "Thank for your love XD!!";
+
+}
